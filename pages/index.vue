c654b7cd04cbe02f443b8873a57e655245cb48f5
Frank Garcia <garcia1901l@gmail.com>
2020-08-02 19:25:36 -0500

    inicial

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h2>Rooms products</h2>
+   <b-list-group>
+
+  <b-list-group-item v-for="(item, index) in rooms" :key="index">
+    <a to=""> Habitación N°: {{ item.name }}</a>
+  </b-list-group-item>
+  
+</b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      'rooms' : []
+    }
+  },
+  async created() {
+    const res = await this.$axios.get('/rooms')
+    this.rooms = res.data
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
